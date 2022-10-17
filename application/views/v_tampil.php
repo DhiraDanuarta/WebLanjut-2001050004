15005bc7e1de485789032064018c60520f5ce71a
DhiraDanuarta <dhiadanuarta@gmail.com>
2022-10-17 16:07:15 +0800

    first commit

---
+++ application/views/v_tampil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TES TAMPIL</title>
+    <link rel="stylesheet" href="<?= base_url("assets/bootstrap/dist/css/bootstrap.css") ?>">
+</head>
+
+<body>
+    <h1>HELLO CODEIGNITER</h1>
+</body>
+
+</html>
